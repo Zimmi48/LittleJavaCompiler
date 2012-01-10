d6258e4214eec0148cae90b8cf5639de7c3abf73
Joris GIOVANNANGELI <joris.giovannangeli@>
2012-01-10 16:27:05 +0100

    Grmph, l'ident ne suffit pas, il faut mettre une expression dans Sattr et Scall
    En effet c'est pas forcément une valeur gauche, j'ai testé avec java,
    
    (new Foo()).x
    
    est valide, si on a une classe foo. Dont acte.

--- src/ast.ml
+++ src/ast.ml
@@ -191,8 +191,8 @@ module Sast = struct
   (** Valeurs gauches *)
   type vars =
     | SVar of ident
-    (** le ident de la classe * le numéros de l'attribut *)
-    | SAttr of ident * int 
+    (** le expr de la classe * le numéros de l'attribut *)
+    | SAttr of expr * int 
 
   (** les méthodes et constructeurs *)
   type callable = {
@@ -219,7 +219,7 @@ module Sast = struct
     (** assigne expr *)
     | SAssign of vars * expr 
     (** Appel d'une méthode, les paramètres sont stockés dans la liste *)
-    | SCall of ident * int * expr list 
+    | SCall of expr * int * expr list 
     (** Accès a une variable (au sens large) *)
     | SGetval of vars 
     (** expression booléene, vrai si expr est une instance de types *)
